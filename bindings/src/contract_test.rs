1fc4897c08974dba4f2a3a5ba888f4a53898ca5f
nikkolasg <nikkolasg@gmail.com>
2022-05-04 16:09:15 +0100

    flatten vs original struct

--- bindings/src/contract_test.rs
+++ bindings/src/contract_test.rs
@@ -18,10 +18,19 @@ mod contracttest_mod {
     use std::sync::Arc;
     pub static CONTRACTTEST_ABI: ethers::contract::Lazy<ethers::core::abi::Abi> =
         ethers::contract::Lazy::new(|| {
-            serde_json :: from_str ("[{\"type\":\"function\",\"name\":\"IS_TEST\",\"inputs\":[],\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\"},{\"type\":\"function\",\"name\":\"failed\",\"inputs\":[],\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\"}],\"stateMutability\":\"view\"},{\"type\":\"function\",\"name\":\"setUp\",\"inputs\":[],\"outputs\":[],\"stateMutability\":\"nonpayable\"},{\"type\":\"function\",\"name\":\"testExample\",\"inputs\":[],\"outputs\":[],\"stateMutability\":\"nonpayable\"},{\"type\":\"event\",\"name\":\"log\",\"inputs\":[{\"name\":\"\",\"type\":\"string\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_address\",\"inputs\":[{\"name\":\"\",\"type\":\"address\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_bytes\",\"inputs\":[{\"name\":\"\",\"type\":\"bytes\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_bytes32\",\"inputs\":[{\"name\":\"\",\"type\":\"bytes32\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_int\",\"inputs\":[{\"name\":\"\",\"type\":\"int256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_address\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"address\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_bytes\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"bytes\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_bytes32\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"bytes32\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_decimal_int\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"int256\",\"indexed\":false},{\"name\":\"decimals\",\"type\":\"uint256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_decimal_uint\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"uint256\",\"indexed\":false},{\"name\":\"decimals\",\"type\":\"uint256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_int\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"int256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_string\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"string\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_named_uint\",\"inputs\":[{\"name\":\"key\",\"type\":\"string\",\"indexed\":false},{\"name\":\"val\",\"type\":\"uint256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_string\",\"inputs\":[{\"name\":\"\",\"type\":\"string\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"log_uint\",\"inputs\":[{\"name\":\"\",\"type\":\"uint256\",\"indexed\":false}],\"anonymous\":false},{\"type\":\"event\",\"name\":\"logs\",\"inputs\":[{\"name\":\"\",\"type\":\"bytes\",\"indexed\":false}],\"anonymous\":false}]") . expect ("invalid abi")
+            serde_json :: from_str ("[{\"inputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"address\",\"name\":\"\",\"type\":\"address\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_address\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"\",\"type\":\"bytes\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_bytes\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"bytes32\",\"name\":\"\",\"type\":\"bytes32\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_bytes32\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"int256\",\"name\":\"\",\"type\":\"int256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_int\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"address\",\"name\":\"val\",\"type\":\"address\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_address\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"bytes\",\"name\":\"val\",\"type\":\"bytes\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_bytes\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"bytes32\",\"name\":\"val\",\"type\":\"bytes32\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_bytes32\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"int256\",\"name\":\"val\",\"type\":\"int256\",\"components\":[],\"indexed\":false},{\"internalType\":\"uint256\",\"name\":\"decimals\",\"type\":\"uint256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_decimal_int\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"uint256\",\"name\":\"val\",\"type\":\"uint256\",\"components\":[],\"indexed\":false},{\"internalType\":\"uint256\",\"name\":\"decimals\",\"type\":\"uint256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_decimal_uint\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"int256\",\"name\":\"val\",\"type\":\"int256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_int\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"string\",\"name\":\"val\",\"type\":\"string\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_string\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"key\",\"type\":\"string\",\"components\":[],\"indexed\":false},{\"internalType\":\"uint256\",\"name\":\"val\",\"type\":\"uint256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_named_uint\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"string\",\"name\":\"\",\"type\":\"string\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_string\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"log_uint\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"bytes\",\"name\":\"\",\"type\":\"bytes\",\"components\":[],\"indexed\":false}],\"type\":\"event\",\"name\":\"logs\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[],\"stateMutability\":\"view\",\"type\":\"function\",\"name\":\"IS_TEST\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\",\"components\":[]}]},{\"inputs\":[],\"stateMutability\":\"view\",\"type\":\"function\",\"name\":\"failed\",\"outputs\":[{\"internalType\":\"bool\",\"name\":\"\",\"type\":\"bool\",\"components\":[]}]},{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\",\"name\":\"setUp\",\"outputs\":[]},{\"inputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\",\"name\":\"testExample\",\"outputs\":[]}]") . expect ("invalid abi")
         });
-    #[derive(Clone)]
+    #[doc = r" Bytecode of the #name contract"]
+    pub static CONTRACTTEST_BYTECODE: ethers::contract::Lazy<ethers::core::types::Bytes> =
+        ethers::contract::Lazy::new(|| {
+            "0x60806040526000805460ff1916600117905534801561001d57600080fd5b5060b48061002c6000396000f3fe6080604052348015600f57600080fd5b506004361060465760003560e01c80630a9254e414604b5780633f5a4a2a14604b578063ba414fa614604d578063fa7626d4146072575b600080fd5b005b600054605e90610100900460ff1681565b604051901515815260200160405180910390f35b600054605e9060ff168156fea26469706673582212205a1ae98fd6987c89802c1b3501202ddfc537a74636d1a29bd160f998a689ea4064736f6c634300080a0033" . parse () . expect ("invalid bytecode")
+        });
     pub struct ContractTest<M>(ethers::contract::Contract<M>);
+    impl<M> Clone for ContractTest<M> {
+        fn clone(&self) -> Self {
+            ContractTest(self.0.clone())
+        }
+    }
     impl<M> std::ops::Deref for ContractTest<M> {
         type Target = ethers::contract::Contract<M>;
         fn deref(&self) -> &Self::Target {
@@ -43,9 +52,46 @@ mod contracttest_mod {
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            let contract =
-                ethers::contract::Contract::new(address.into(), CONTRACTTEST_ABI.clone(), client);
-            Self(contract)
+            ethers::contract::Contract::new(address.into(), CONTRACTTEST_ABI.clone(), client).into()
+        }
+        #[doc = r" Constructs the general purpose `Deployer` instance based on the provided constructor arguments and sends it."]
+        #[doc = r" Returns a new instance of a deployer that returns an instance of this contract after sending the transaction"]
+        #[doc = r""]
+        #[doc = r" Notes:"]
+        #[doc = r" 1. If there are no constructor arguments, you should pass `()` as the argument."]
+        #[doc = r" 1. The default poll duration is 7 seconds."]
+        #[doc = r" 1. The default number of confirmations is 1 block."]
+        #[doc = r""]
+        #[doc = r""]
+        #[doc = r" # Example"]
+        #[doc = r""]
+        #[doc = r" Generate contract bindings with `abigen!` and deploy a new contract instance."]
+        #[doc = r""]
+        #[doc = r" *Note*: this requires a `bytecode` and `abi` object in the `greeter.json` artifact."]
+        #[doc = r""]
+        #[doc = r" ```ignore"]
+        #[doc = r" # async fn deploy<M: ethers::providers::Middleware>(client: ::std::sync::Arc<M>) {"]
+        #[doc = r#"     abigen!(Greeter,"../greeter.json");"#]
+        #[doc = r""]
+        #[doc = r#"    let greeter_contract = Greeter::deploy(client, "Hello world!".to_string()).unwrap().send().await.unwrap();"#]
+        #[doc = r"    let msg = greeter_contract.greet().call().await.unwrap();"]
+        #[doc = r" # }"]
+        #[doc = r" ```"]
+        pub fn deploy<T: ethers::core::abi::Tokenize>(
+            client: ::std::sync::Arc<M>,
+            constructor_args: T,
+        ) -> Result<
+            ethers::contract::builders::ContractDeployer<M, Self>,
+            ethers::contract::ContractError<M>,
+        > {
+            let factory = ethers::contract::ContractFactory::new(
+                CONTRACTTEST_ABI.clone(),
+                CONTRACTTEST_BYTECODE.clone().into(),
+                client,
+            );
+            let deployer = factory.deploy(constructor_args)?;
+            let deployer = ethers::contract::ContractDeployer::new(deployer);
+            Ok(deployer)
         }
         #[doc = "Calls the contract's `IS_TEST` (0xfa7626d4) function"]
         pub fn is_test(&self) -> ethers::contract::builders::ContractCall<M, bool> {
@@ -156,6 +202,11 @@ mod contracttest_mod {
         #[doc = r" Returns an [`Event`](#ethers_contract::builders::Event) builder for all events of this contract"]
         pub fn events(&self) -> ethers::contract::builders::Event<M, ContractTestEvents> {
             self.0.event_with_filter(Default::default())
+        }
+    }
+    impl<M: ethers::providers::Middleware> From<ethers::contract::Contract<M>> for ContractTest<M> {
+        fn from(contract: ethers::contract::Contract<M>) -> Self {
+            Self(contract)
         }
     }
     #[derive(
--- bindings/src/my_contract.rs
+++ bindings/src/my_contract.rs
@@ -17,9 +17,20 @@ mod mycontract_mod {
     #[doc = "MyContract was auto-generated with ethers-rs Abigen. More information at: https://github.com/gakonst/ethers-rs"]
     use std::sync::Arc;
     pub static MYCONTRACT_ABI: ethers::contract::Lazy<ethers::core::abi::Abi> =
-        ethers::contract::Lazy::new(|| serde_json::from_str("[]").expect("invalid abi"));
-    #[derive(Clone)]
+        ethers::contract::Lazy::new(|| {
+            serde_json :: from_str ("[{\"inputs\":[{\"internalType\":\"struct MyContract.Point\",\"name\":\"x\",\"type\":\"tuple\",\"components\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\",\"components\":[]},{\"internalType\":\"uint256\",\"name\":\"y\",\"type\":\"uint256\",\"components\":[]}],\"indexed\":false}],\"type\":\"event\",\"name\":\"NewPoint\",\"outputs\":[],\"anonymous\":false},{\"inputs\":[{\"internalType\":\"struct MyContract.Point\",\"name\":\"_point\",\"type\":\"tuple\",\"components\":[{\"internalType\":\"uint256\",\"name\":\"x\",\"type\":\"uint256\",\"components\":[]},{\"internalType\":\"uint256\",\"name\":\"y\",\"type\":\"uint256\",\"components\":[]}]}],\"stateMutability\":\"nonpayable\",\"type\":\"function\",\"name\":\"submitPoint\",\"outputs\":[]}]") . expect ("invalid abi")
+        });
+    #[doc = r" Bytecode of the #name contract"]
+    pub static MYCONTRACT_BYTECODE: ethers::contract::Lazy<ethers::core::types::Bytes> =
+        ethers::contract::Lazy::new(|| {
+            "0x608060405234801561001057600080fd5b5061010f806100206000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063b3c67bb814602d575b600080fd5b603c6038366004607e565b603e565b005b6040805182518152602080840151908201527f0a10caaea7ac3c68834bca5fb5f42a10cb68bc3866ed86e6379d62bea6d59166910160405180910390a150565b600060408284031215608f57600080fd5b6040516040810181811067ffffffffffffffff8211171560bf57634e487b7160e01b600052604160045260246000fd5b60405282358152602092830135928101929092525091905056fea2646970667358221220e8dcb1b728de44f1af0c6f1d2a334e49bb92560b6fe4817dce7a5bc16d50610364736f6c634300080a0033" . parse () . expect ("invalid bytecode")
+        });
     pub struct MyContract<M>(ethers::contract::Contract<M>);
+    impl<M> Clone for MyContract<M> {
+        fn clone(&self) -> Self {
+            MyContract(self.0.clone())
+        }
+    }
     impl<M> std::ops::Deref for MyContract<M> {
         type Target = ethers::contract::Contract<M>;
         fn deref(&self) -> &Self::Target {
@@ -41,9 +52,109 @@ mod mycontract_mod {
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            let contract =
-                ethers::contract::Contract::new(address.into(), MYCONTRACT_ABI.clone(), client);
+            ethers::contract::Contract::new(address.into(), MYCONTRACT_ABI.clone(), client).into()
+        }
+        #[doc = r" Constructs the general purpose `Deployer` instance based on the provided constructor arguments and sends it."]
+        #[doc = r" Returns a new instance of a deployer that returns an instance of this contract after sending the transaction"]
+        #[doc = r""]
+        #[doc = r" Notes:"]
+        #[doc = r" 1. If there are no constructor arguments, you should pass `()` as the argument."]
+        #[doc = r" 1. The default poll duration is 7 seconds."]
+        #[doc = r" 1. The default number of confirmations is 1 block."]
+        #[doc = r""]
+        #[doc = r""]
+        #[doc = r" # Example"]
+        #[doc = r""]
+        #[doc = r" Generate contract bindings with `abigen!` and deploy a new contract instance."]
+        #[doc = r""]
+        #[doc = r" *Note*: this requires a `bytecode` and `abi` object in the `greeter.json` artifact."]
+        #[doc = r""]
+        #[doc = r" ```ignore"]
+        #[doc = r" # async fn deploy<M: ethers::providers::Middleware>(client: ::std::sync::Arc<M>) {"]
+        #[doc = r#"     abigen!(Greeter,"../greeter.json");"#]
+        #[doc = r""]
+        #[doc = r#"    let greeter_contract = Greeter::deploy(client, "Hello world!".to_string()).unwrap().send().await.unwrap();"#]
+        #[doc = r"    let msg = greeter_contract.greet().call().await.unwrap();"]
+        #[doc = r" # }"]
+        #[doc = r" ```"]
+        pub fn deploy<T: ethers::core::abi::Tokenize>(
+            client: ::std::sync::Arc<M>,
+            constructor_args: T,
+        ) -> Result<
+            ethers::contract::builders::ContractDeployer<M, Self>,
+            ethers::contract::ContractError<M>,
+        > {
+            let factory = ethers::contract::ContractFactory::new(
+                MYCONTRACT_ABI.clone(),
+                MYCONTRACT_BYTECODE.clone().into(),
+                client,
+            );
+            let deployer = factory.deploy(constructor_args)?;
+            let deployer = ethers::contract::ContractDeployer::new(deployer);
+            Ok(deployer)
+        }
+        #[doc = "Calls the contract's `submitPoint` (0xb3c67bb8) function"]
+        pub fn submit_point(
+            &self,
+            point: Point,
+        ) -> ethers::contract::builders::ContractCall<M, ()> {
+            self.0
+                .method_hash([179, 198, 123, 184], (point,))
+                .expect("method not found (this should never happen)")
+        }
+        #[doc = "Gets the contract's `NewPoint` event"]
+        pub fn new_point_filter(&self) -> ethers::contract::builders::Event<M, NewPointFilter> {
+            self.0.event()
+        }
+        #[doc = r" Returns an [`Event`](#ethers_contract::builders::Event) builder for all events of this contract"]
+        pub fn events(&self) -> ethers::contract::builders::Event<M, NewPointFilter> {
+            self.0.event_with_filter(Default::default())
+        }
+    }
+    impl<M: ethers::providers::Middleware> From<ethers::contract::Contract<M>> for MyContract<M> {
+        fn from(contract: ethers::contract::Contract<M>) -> Self {
             Self(contract)
         }
+    }
+    #[derive(
+        Clone,
+        Debug,
+        Default,
+        Eq,
+        PartialEq,
+        ethers :: contract :: EthEvent,
+        ethers :: contract :: EthDisplay,
+    )]
+    #[ethevent(name = "NewPoint", abi = "NewPoint((uint256,uint256))")]
+    pub struct NewPointFilter {
+        pub x: (ethers::core::types::U256, ethers::core::types::U256),
+    }
+    #[doc = "Container type for all input parameters for the `submitPoint`function with signature `submitPoint((uint256,uint256))` and selector `[179, 198, 123, 184]`"]
+    #[derive(
+        Clone,
+        Debug,
+        Default,
+        Eq,
+        PartialEq,
+        ethers :: contract :: EthCall,
+        ethers :: contract :: EthDisplay,
+    )]
+    #[ethcall(name = "submitPoint", abi = "submitPoint((uint256,uint256))")]
+    pub struct SubmitPointCall {
+        pub point: Point,
+    }
+    #[doc = "`Point(uint256,uint256)`"]
+    #[derive(
+        Clone,
+        Debug,
+        Default,
+        Eq,
+        PartialEq,
+        ethers :: contract :: EthAbiType,
+        ethers :: contract :: EthAbiCodec,
+    )]
+    pub struct Point {
+        pub x: ethers::core::types::U256,
+        pub y: ethers::core::types::U256,
     }
 }
